e93587fce7993e5de2987d6f049984e58f5bd521
yemibox51 <yemibox51@gmail.com>
2022-02-19 18:31:28 -0500

    Spin button
    Spin button works when clicked but need to check if the code is actually spinning the wheel

--- spin-to-win/script.js
+++ spin-to-win/script.js
@@ -1,15 +1,28 @@
-const spin = function() {
-    
-    const wheel = document.querySelector('.wheel');
-    const startButton = document.querySelector('.button');
-    let degree = 0;
-    
-    
-    startButton.addEventListener('click', () => {
+const wheel = document.querySelector('.wheel');
+const startButton = document.querySelector('.button');
+let degree = 0;
+
+function spin() {
+    /* startButton.addEventListener('click', () => {
+        // Need to disable button after the user has clicked it
+        startButton.style.pointerEvents = 'none';
+        
+        console.log('I spun');
+        
+        // Calculate the spin of the circle
+        degree = Math.floor(250 + Math.random() * 250);
+        
+        // Want the wheel to start moving but slow down near the end.
+        wheel.style.transition = 'all 10s eas-out';
+        wheel.style.transform = `rotate(${degree}degree)`;
         
     });
+    */
     
+    console.log('I spun');
     wheel.addEventListener('transitionend', () => {
         
     });
 }
+
+console.log('What up?');
