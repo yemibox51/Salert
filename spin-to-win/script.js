545626e17403a37d4e16eb3823346b110e8a309f
yemibox51 <yemibox51@gmail.com>
2022-02-19 18:08:39 -0500

    Basic Spin functionality
    Getting the button to spin the wheel.

---
+++ spin-to-win/script.js
@@ -0,0 +1,15 @@
+const spin = function() {
+    
+    const wheel = document.querySelector('.wheel');
+    const startButton = document.querySelector('.button');
+    let degree = 0;
+    
+    
+    startButton.addEventListener('click', () => {
+        
+    });
+    
+    wheel.addEventListener('transitionend', () => {
+        
+    });
+}
